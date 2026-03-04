d44864b61d5e8aee4239c28e26efb83067094576
Александр2.0 <pranjalpanta6@gmail.com>
2026-03-04 22:12:52 +0545

    Create keystore.py

---
+++ keystore.py
@@ -0,0 +1,55 @@
+import json
+import os
+import base64
+from cryptography import x509
+from cryptography.hazmat.primitives import serialization
+from src.security.crypto_engine import CryptoEngine 
+
+class SecureKeyStore:
+    """Manages the encrypted storage of the user's Private Key."""
+    
+    def __init__(self, user_email):
+        self.filename = f"{user_email.replace('@', '_at_')}.keystore"
+
+    def save_keys(self, private_key, certificate, password):
+        pem_priv = private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption() 
+        )
+        
+        salt = os.urandom(16)
+        wrapping_key = CryptoEngine.derive_key_argon2(password, salt)
+        nonce, ciphertext = CryptoEngine.aes_gcm_encrypt(pem_priv, wrapping_key)
+
+        keystore_data = {
+            "salt": base64.b64encode(salt).decode('utf-8'),
+            "nonce": base64.b64encode(nonce).decode('utf-8'),
+            "encrypted_key": base64.b64encode(ciphertext).decode('utf-8'),
+            "certificate": base64.b64encode(certificate.public_bytes(serialization.Encoding.PEM)).decode('utf-8')
+        }
+
+        with open(self.filename, "w") as f:
+            json.dump(keystore_data, f, indent=4)
+
+    def load_keys(self, password):
+        if not os.path.exists(self.filename):
+            raise FileNotFoundError(f"Keystore file not found for this user: {self.filename}")
+
+        with open(self.filename, "r") as f: data = json.load(f)
+
+        salt = base64.b64decode(data["salt"])
+        nonce = base64.b64decode(data["nonce"])
+        ciphertext = base64.b64decode(data["encrypted_key"])
+        cert_bytes = base64.b64decode(data["certificate"])
+
+        try:
+            wrapping_key = CryptoEngine.derive_key_argon2(password, salt)
+            pem_priv = CryptoEngine.aes_gcm_decrypt(nonce, ciphertext, wrapping_key)
+            
+            private_key = serialization.load_pem_private_key(pem_priv, password=None)
+            certificate = x509.load_pem_x509_certificate(cert_bytes)
+            
+            return private_key, certificate
+        except Exception:
+            raise PermissionError("Keystore Access Denied: Wrong Password.") 
